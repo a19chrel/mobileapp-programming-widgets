8ff148376635398e2ff2a36ff94ec4761717d908
a19chrel <a19chrel@student.his.se>
2020-04-14 12:35:32 +0200

    Added: ListView adapter and data.

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -5,16 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Data for listView
+    private String[] names = {"Super Berget", "Berget Klara", "Berg x100"};
+
+    private ArrayList<String> listData = new ArrayList<String>(Arrays.asList(names));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //ListView
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(),R.layout.listitem_layout,R.id.listitem_layout,listData);
+        ListView myListView = findViewById(R.id.listview);
+        myListView.setAdapter(adapter);
 
+
+        //Buttons
         Button like = findViewById(R.id.button);
         Button dislike = findViewById(R.id.button2);
 
